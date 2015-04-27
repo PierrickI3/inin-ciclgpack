4fa9d73b4e9a0e4868fca3fa49b463afb31e296b
PierrickI3 <pierrick.lozach@inin.com>
2015-04-27 16:56:19 +0200

    Adding correct dependencies Adding logic to support all known language packs

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,8 @@
 #
 # [locale]
 #   Country and language of the language packs to install
-#   e.g. "fr-FR" for French (France) or "en-UK" for English (United Kingdom)
-#   Supported locales are here: https://my.inin.com/products/cic/Pages/Localization.aspx
+#   e.g. "fr" for French (France) or "en-UK" for English (United Kingdom) or zh-Hant for Chinese (Taiwan)
+#   Supported locales are here: https://my.inin.com/products/cic/Pages/Localization.aspx and in the readme.MD file
 #
 # [cic_version]
 #   Current version of CIC (i.e. "2015_R2")
@@ -16,7 +16,7 @@
 #
 #  class { 'ciclgpack':
 #    ensure      => installed,
-#    locales     => [ 'fr-FR', 'en-UK', 'nl-NL' ],
+#    locale      => 'fr',
 #    cic_version => '2015_R2',
 #  }
 #
@@ -30,7 +30,7 @@
 #
 class ciclgpack (
   $ensure,
-  $locales,
+  $locale,
   $cic_version,
 )
 {
@@ -45,19 +45,49 @@ class ciclgpack (
     fail('Unsupported OS')
   }
 
-  case $locale
+  case downcase($locale)
   {
-    'fr-FR':
+    'en-au':
     {
-      $currentlanguagepackmsi = "${languagepackmsi}_fr_${cic_version}.msi"
+      $currentlanguagepackmsi = "${languagepackmsi}_en_AU_${cic_version}.msi"
+    }
+    'en-gb':
+    {
+      $currentlanguagepackmsi = "${languagepackmsi}_en_GB_${cic_version}.msi"
+    }
+    'en-nz':
+    {
+      $currentlanguagepackmsi = "${languagepackmsi}_en_NZ_${cic_version}.msi"
+    }
+    'es-es':
+    {
+      $currentlanguagepackmsi = "${languagepackmsi}_es_ES_${cic_version}.msi"
+    }
+    'fr-ca':
+    {
+      $currentlanguagepackmsi = "${languagepackmsi}_fr_CA_${cic_version}.msi"
+    }
+    'pt-br':
+    {
+      $currentlanguagepackmsi = "${languagepackmsi}_pt_BR_${cic_version}.msi"
+    }
+    'zh-hans':
+    {
+      $currentlanguagepackmsi = "${languagepackmsi}_zh_Hans_${cic_version}.msi"
+    }
+    'zh-hant':
+    {
+      $currentlanguagepackmsi = "${languagepackmsi}_zh_Hant_${cic_version}.msi"
     }
     default:
     {
-      fail("Language ${locale} not supported")
+      $currentlanguagepackmsi = "${languagepackmsi}_${locale}_${cic_version}.msi"
     }
   }
 
-case $ensure
+  debug("Installing Language Pack for ${locale}. MSI: ${currentlanguagepackmsi}")
+
+  case $ensure
   {
     installed:
     {
@@ -69,49 +99,44 @@ case $ensure
         cwd     => $::system32,
         creates => "${mountdriveletter}/Installs/Install.exe",
         timeout => 30,
-        before  => Exec['unmount-cic-iso'],
       }
 
       debug("Installing Language Pack for ${locale}")
-      package {"language-pack-install":
+      package {'language-pack-install':
         ensure          => installed,
-        source          => "${daascache}\\${currentlanguagepackmsi}",
-        install_options => [  '/qn',
-                              '/norestart',
-                              {'STARTEDBYEXEORIUPDATE' => '1'},
-                              {'REBOOT' => 'ReallySuppress'},
-                          ],
+        source          => "${mountdriveletter}\\Installs\\LanguagePacks\\${currentlanguagepackmsi}",
+        install_options => [ {'STARTEDBYEXEORIUPDATE' => '1'}, {'REBOOT' => 'ReallySuppress'}, ],
         require         => Exec['mount-cic-iso'],
       }
 
-      debug('Adding media server analysis language model instructions on desktop')
+      debug('Adding instructions on desktop')
       file {'C:/Users/Vagrant/Desktop/MediaServer Language Analysis Instructions.txt':
         ensure  => present,
-        content => 'To configure Media Server speech for your language, create or update the "Call Analysis Language" server parameter to one of the values listed on page 67 of the following document: https://my.inin.com/products/cic/Documentation/mergedProjects/wh_tr/bin/media_server_tr.pdf',
-        after   => Package["language-pack-install-${locale}"],
-        require => Exec['mount-cic-iso'],
+        content => 'To configure Media Server speech for your language, create or update the "Call Analysis Language" server parameter to one of the values listed on page 67 of the following document: https://my.inin.com/products/cic/Documentation/mergedProjects/wh_tr/bin/media_server_tr.pdf\n\rYou can also download dial plans from https://my.inin.com/products/cic/Pages/Localization.aspx under the "Localized Dial Plans" section at the bottom of the page',
+        require => Package['language-pack-install'],
       }
 
       debug("Setting Windows Culture to ${locale}")
-      exec {"set-windows-culture":
+      exec {'set-windows-culture':
         command  => "Set-Culture ${locale}",
         provider => powershell,
         path     => $::path,
         cwd      => $::system32,
         timeout  => 30,
-        require  => Package["language-pack-install"],
+        require  => Package['language-pack-install'],
       }
-
-      # Create a localized dial plan? https://my.inin.com/products/cic/Pages/Localization.aspx
 
       # Unmount CIC ISO
       debug('Unmounting CIC ISO')
       exec {'unmount-cic-iso':
-        command => "cmd.exe /c imdisk -d -m ${mountdriveletter}",
+        command => "cmd.exe /c imdisk -D -m ${mountdriveletter}",
         path    => $::path,
         cwd     => $::system32,
         timeout => 30,
-        require => Exec['set-windows-culture'],
+        require => [
+                    Exec['mount-cic-iso'],
+                    Exec['set-windows-culture'],
+                  ],
       }
     }
     default:
