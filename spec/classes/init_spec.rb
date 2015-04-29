09308efde5a2ce704996aed3c8b89721ba8b058c
PierrickI3 <pierrick.lozach@inin.com>
2015-04-29 18:01:38 +0200

    Mo' rspec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,11 @@
+require 'puppetlabs_spec_helper/module_spec_helper'
 require 'spec_helper'
-describe 'ciclgpack' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('ciclgpack') }
-  end
+describe 'ciclgpack' do
+  let(:title) {'ciclgpack'}
+
+  let(:params) { {:locale => 'fr', :cic_version => '2015_R2'} }
+  let(:facts) { { :operatingsystem => 'Windows'} }
+
+  it { is_expected.to create_class('ciclgpack') }
 end
