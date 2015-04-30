14b4501764e253676b6c1a5e998e771e62fcff1f
PierrickI3 <pierrick.lozach@inin.com>
2015-04-30 18:13:53 +0200

    Adding ensure

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -4,9 +4,8 @@ require 'spec_helper'
 describe 'ciclgpack' do
   let(:title) {'ciclgpack'}
 
-  let(:params) { {:locale => 'fr', :cic_version => '2015_R2'} }
+  let(:params) { {:ensure => 'installed', :locale => 'fr', :cic_version => '2015_R2'} }
   let(:facts) { { :operatingsystem => 'Windows'} }
-
 
   it { is_expected.to create_class('ciclgpack') }
 end
