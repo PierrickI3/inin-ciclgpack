24b794b019acb51d4adfb10da3b6a6fb46957d10
PierrickI3 <pierrick.lozach@inin.com>
2015-04-30 18:08:50 +0200

    Fixing merging issue

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,16 +7,6 @@ describe 'ciclgpack' do
   let(:params) { {:locale => 'fr', :cic_version => '2015_R2'} }
   let(:facts) { { :operatingsystem => 'Windows'} }
 
-<<<<<<< HEAD
-  context 'with defaults for all parameters' do
-  	let(:facts) { {:operatingsystem => 'Windows'} }
-  	let(:params) { {:ensure => 'installed', :locale => 'fr', :cic_version => '2015_R2'} }
-    it { 
-    	should contain_class('ciclgpack')
-    }
-  end
-end
-=======
+
   it { is_expected.to create_class('ciclgpack') }
 end
->>>>>>> 09308efde5a2ce704996aed3c8b89721ba8b058c
