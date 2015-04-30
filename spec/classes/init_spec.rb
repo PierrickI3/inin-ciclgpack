747156bd6ab7ba85237251b83ccf6ed1966fa5b4
PierrickI3 <pierrick.lozach@inin.com>
2015-04-30 14:44:29 +0200

    Adding RSpec files

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
+
 describe 'ciclgpack' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('ciclgpack') }
+  	let(:facts) { {:operatingsystem => 'Windows'} }
+  	let(:params) { {:ensure => 'installed', :locale => 'fr', :cic_version => '2015_R2'} }
+    it { 
+    	should contain_class('ciclgpack')
+    }
   end
 end
